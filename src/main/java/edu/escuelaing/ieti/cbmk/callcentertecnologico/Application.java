f8f03c221dde5ece0e9648a620d17f9ae696cd98
Cristhian Torres <cristhian.torres@mail.escuelaing.edu.co>
2022-02-16 23:10:08 -0500

    Esqueleto

--- src/main/java/edu/escuelaing/ieti/cbmk/callcentertecnologico/Application.java
+++ src/main/java/edu/escuelaing/ieti/cbmk/callcentertecnologico/Application.java
@@ -1,4 +1,4 @@
-package edu.escuelaing.ieti.cbmk.callcentertecnol.gico;
+package edu.escuelaing.ieti.cbmk.callcentertecnologico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
