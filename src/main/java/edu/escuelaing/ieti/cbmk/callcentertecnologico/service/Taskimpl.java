20c274f1cca8ee45092e82c3f1a79ab10ab2ebd3
Matqui <matqui@metrocuadrado.com>
2022-03-30 23:51:04 -0500

    comentar codigo mientras integracion con tecnicos

--- src/main/java/edu/escuelaing/ieti/cbmk/callcentertecnologico/service/Taskimpl.java
+++ src/main/java/edu/escuelaing/ieti/cbmk/callcentertecnologico/service/Taskimpl.java
@@ -1,32 +1,46 @@
 package edu.escuelaing.ieti.cbmk.callcentertecnologico.service;
 
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
-import edu.escuelaing.ieti.cbmk.callcentertecnologico.Repository.taskRepository;
-import edu.escuelaing.ieti.cbmk.callcentertecnologico.model.TaskModel;
+import edu.escuelaing.ieti.cbmk.callcentertecnologico.Repository.*;
+import edu.escuelaing.ieti.cbmk.callcentertecnologico.model.*;
 
 public class Taskimpl implements Task {
 	
 	@Autowired
     private taskRepository taskRepository;
-	private TecnicoService TecnicoService;
+	/*private TecnicoService TecnicoService;*/
 
 	@Override
 	public TaskModel createTask(TaskModel task) {
-		String eleccionTecnico = escogerTecnico(task);
-		task.setTecnicoId(eleccionTecnico);
+		/*String eleccionTecnico = escogerTecnico(task);*/
+		/*task.setTecnicoId(eleccionTecnico);*/
 		return taskRepository.save(task);
 	}
 
-	private String escogerTecnico(TaskModel task) {
+	/*private String escogerTecnico(TaskModel task) {
 		List<Tecnico> tecnicos = TecnicoService.findAll();
 		for (Tecnico tecnico : tecnicos) {
 			if (tecnico.getSkill() = task.getSkill() && tecnico.getActivate().equals("A")) {
 				return tecnico.getId();
 			}
 			
-		}
+		}	
+	}*/
+
+	@Override
+	public List<TaskModel> AllTask() {
+		return taskRepository.findAll();
+		 
+	}
+
+	@Override
+	public Optional<TaskModel> TaskbyId(String id) {
+		return taskRepository.findById(id);
 	}
 
 }
